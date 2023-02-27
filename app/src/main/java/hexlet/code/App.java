7371ebd9dd08bb87e5bf4a1bfad1a2bf4863c435
Anna Borisova <AnnaBB@yandex.ru>
2023-02-27 18:26:47 +0400

    add EvenGame

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,39 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
+    static Scanner scanner = new Scanner(System.in);
+    static String userName;
+    static String userChoice;
     public static void main(String[] args) {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+
+        userChoice = scanner.next();
+        System.out.println("Your choice: " + userChoice);
+        switch (userChoice) {
+            case "0":
+                System.out.println("Goodbye");
+                break;
+            case "1":
+                greet();
+                break;
+            case "2":
+                greet();
+                EvenGame.play();
+                break;
+        }
+
+        scanner.close();
+    }
+
+    public static void greet() {
         System.out.println("Welcome to the Brain Games!");
-        Cli.greet();
+        System.out.print("What is your name? ");
+        userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
     }
 }
---
+++ app/src/main/java/hexlet/code/EvenGame.java
@@ -0,0 +1,44 @@
+package hexlet.code;
+public class EvenGame {
+    public static void play() {
+        int i = 0;
+        boolean userAnswerCheck = true;
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        while ((i < 3) && (userAnswerCheck)) {
+
+            int number = (int) (Math.random() * 100);
+            System.out.println("Question: " + number);
+            System.out.print("Your answer: ");
+            String userAnswer = App.scanner.next();
+
+            if ((isEven(number)) && (userAnswer.equalsIgnoreCase("yes"))) {
+                System.out.println("Correct!");
+                i++;
+            } else if (!(isEven(number)) && (userAnswer.equalsIgnoreCase("no"))) {
+                System.out.println("Correct!");
+                i++;
+            } else {
+                if (isEven(number)) {
+                    System.out.println("\'" + userAnswer + "\'" + " is wrong answer ;(. Correct answer was 'yes'.");
+                } else {
+                    System.out.println("\'" + userAnswer + "\'" + " is wrong answer ;(. Correct answer was 'no'.");
+                }
+                userAnswerCheck = false;
+            }
+        }
+
+        if (i == 3) {
+            System.out.println("Congratulations, " + App.userName + "!");
+        }
+    }
+
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
